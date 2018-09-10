ee9fe9a794c9681921191514370eb73883c9e800
Doken Edgar <dokenedgarjr@gmail.com>
2018-09-10 18:34:41 +0100

    added airbnb style guide and linter

--- test/firstTest.js
+++ test/firstTest.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
-describe('suite', function () {
-  it('expectation', function (done) {
+//describe('suite', function () {
+ // it('expectation', function (done) {
    // assert.equal(23, 23, "[message]");
-  });
-});
+  //});
+//});
