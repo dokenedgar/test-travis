d62f5a9204eb0d383d03b95ad262ef8c789ede7f
Doken Edgar <dokenedgarjr@gmail.com>
2018-09-14 01:46:42 +0100

    added istanbul and nyc

--- test/firstTest.js
+++ test/firstTest.js
@@ -1,7 +1,10 @@
+"use strict";
+//import sum from './secTest.js';
+let add = require('../secTest.js');
 const assert = require('assert');
 
-//describe('suite', function () {
- // it('expectation', function (done) {
-   // assert.equal(23, 23, "[message]");
-  //});
-//});
+describe('suite', function () {
+  it('expectation', function () {
+    assert.equal(add(1,2), 3);
+  });
+});
